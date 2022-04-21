72d69bf70a9e96af86f3081851a77c99d5b2a171
Netinho <jcarlosneto1991@gmail.com>
2022-04-20 23:41:19 -0300

    aula de composicao

---
+++ curso-java/src/entities/Worker.java
@@ -0,0 +1,92 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+
+	private String name;
+	private WorkerLevel level;
+	private Double baseSalary;
+	
+	private Department department;
+	private List<HourContract> contracts = new ArrayList<HourContract>();
+	
+	public Worker() {
+		
+	}
+
+	public Worker(String name, WorkerLevel level, Double salary, Department department) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = salary;
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getSalary() {
+		return baseSalary;
+	}
+
+	public void setSalary(Double salary) {
+		this.baseSalary = salary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	@Override
+	public String toString() {
+		return "Worker [name=" + name + ", level=" + level + ", salary=" + baseSalary + ", department=" + department.getName()
+				+ ", contracts=" + contracts + "]";
+	}
+	
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public double income(int year, int mouth) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for(HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if(year == c_year && mouth == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
+}
