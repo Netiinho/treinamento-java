72b6fc9ce83a63707500879a0166955b43ee0a14
Netinho <jcarlosneto1991@gmail.com>
2022-04-28 19:17:53 -0300

    commit

--- curso-java/src/application/Program.java
+++ curso-java/src/application/Program.java
@@ -1,10 +1,70 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import enums.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter cliente data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		sc.nextLine();
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		String status = sc.nextLine();
+		System.out.print("How many items to this order: ");
+		int items = sc.nextInt();
+		
+		Client client = new Client(name, email, birthDate);
+		
+		Date orderDate = new Date();
+		
+		Order order = new Order(orderDate, OrderStatus.valueOf(status), client);
+		
+		OrderItem orderItem = new OrderItem();
+		
+		for(int i=1; i<=items; i++) {
+			sc.nextLine();
+			System.out.println("Enter #" + i + " item data:");
+			System.out.println("Product Name: ");
+			String nameItem = sc.nextLine();
+			orderItem.setName(nameItem);
+			
+			System.out.println("Product Price: ");
+			double priceItem = sc.nextDouble();
+			orderItem.setPrice(priceItem);
+			
+			System.out.println("Quantity: ");
+			Integer quantity = sc.nextInt();
+			orderItem.setQuantity(quantity);
+			
+			order.addItems(orderItem);
+		}
+		
+		System.out.print(order.toString());
+		
+		sc.close();
 	}
-
 }
---
+++ curso-java/src/entities/OrderItem.java
@@ -0,0 +1,72 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderItem {
+	
+	private int quantity;
+	private double price;
+	private String name;
+	
+	
+	public OrderItem() {
+		
+	}
+
+
+	public OrderItem(int quantity, double price, String name) {
+		this.quantity = quantity;
+		this.price = price;
+		this.name = name;
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double subTotal() {
+		double subTotal = price * quantity;
+		return subTotal;
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(name);
+		sb.append(", ");
+		sb.append(price);
+		sb.append(", ");
+		sb.append("Quantity: " + quantity);
+		sb.append(", ");
+		sb.append("Subtotal: " + price + "\n");
+		return sb.toString();
+	}
+}
